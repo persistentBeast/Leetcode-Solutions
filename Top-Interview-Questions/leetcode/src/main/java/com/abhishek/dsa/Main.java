2b8d094fbc151efbe5095553eba12a922ac8b6b9
abhishekbhalla <abhishekbhalla9801@gmail.com>
2024-05-11 21:59:42 +0530

    weekly dsa

--- Top-Interview-Questions/leetcode/src/main/java/com/abhishek/dsa/Main.java
+++ Top-Interview-Questions/leetcode/src/main/java/com/abhishek/dsa/Main.java
@@ -1,13 +1,40 @@
 package com.abhishek.dsa;
 
-import main.java.com.abhishek.dsa.Solution;
+
+import java.util.NavigableSet;
+import java.util.PriorityQueue;
+import java.util.SortedSet;
+import java.util.TreeSet;
 
 public class Main {
     public static void main(String[] args) {
 
-        Solution solution = new Solution();
 
-        System.out.println(solution.majorityElement(new int[]{0,0,1,1,1,1,2,3,3,3,3,3,3,3,3,3}));
+//        PriorityQueue<Integer> minPq = new PriorityQueue<>();
+//        minPq.add(10);
+//        minPq.add(10);
+//        System.out.println(minPq.size());
+//        minPq.remove(10);
+//        System.out.println(minPq.size());
+//
+//        NavigableSet<Integer> set = new TreeSet<>();
+//
+//        // Add some elements to the set
+//        set.add(10);
+//        set.add(20);
+//        set.add(30);
+//        set.add(40);
+//        set.add(50);
+//        System.out.println(set.getFirst());
+//        System.out.println(set.getLast());
+
+        TreeSet<Integer> set = new TreeSet<>();
+        set.add(1);
+        set.add(1);
+
+        System.out.println(set.size());
+
+
 
     }
 }
--- Top-Interview-Questions/leetcode/src/main/java/com/abhishek/dsa/Solution.java
+++ Top-Interview-Questions/leetcode/src/main/java/com/abhishek/dsa/Solution.java
@@ -1,10 +1,10 @@
-package main.java.com.abhishek.dsa;
+package com.abhishek.dsa;
 
-import java.util.BitSet;
+import java.util.*;
 import java.util.function.BinaryOperator;
 
 public class Solution {
-    
+
 
     //27. Remove Element
     public int removeElement(int[] nums, int val) {
@@ -95,7 +95,7 @@ public class Solution {
         }
 
         return max_profit;
-        
+
     }
 
     //122. Best Time to Buy and Sell Stock II
@@ -116,6 +116,155 @@ public class Solution {
         ans += nums[n-1] - nums[st];
 
         return ans;
-        
+
     }
+
+    //983. Minimum Cost For Tickets
+    public int mincostTickets(int[] days, int[] costs) {
+
+        int n = days.length;
+
+        int [] dp = new int[365];
+
+        Arrays.fill(dp, -1);
+
+        for(int i = 0; i < n; i++){
+
+            int c1 = costs[0];
+
+            int c2 = costs[1];
+            for(int j = 1; j < 7; j++){
+                if(days[i] - j >= 0 && dp[days[i] - j] != -1){
+                    c2 = Math.min(c2, dp[days[i] - j] + costs[1]);
+                }
+            }
+
+            int c3 = costs[2];
+            for(int j = 1; j < 30; j++){
+                if(days[i] - j >= 0 && dp[days[i] - j] != -1){
+                    c3 = Math.min(c3, dp[days[i] - j] + costs[2]);
+                }
+            }
+
+            dp[days[i]] = Math.min(c1, Math.min(c2, c3)) + ( i - 1 >= 0? dp[days[i-1]] : 0);
+
+        }
+
+        return dp[days[n-1]];
+
+    }
+
+    //3075. Maximize Happiness of Selected Children
+    public long maximumHappinessSum(int[] happiness, int k) {
+        int n = happiness.length, pen = 0;
+        long ans = 0;
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Comparator.reverseOrder());
+        for(int i = 0; i < n; i++){
+            pq.add(happiness[i]);
+        }
+        for(int i = 0; i < k; i++){
+            Integer tp = pq.poll();
+            tp -= pen;
+            tp = Math.max(0, tp);
+            ans += tp;
+            pen++;
+        }
+        return ans;
+    }
+
+    //3075. Maximize Happiness of Selected Children
+    public long maximumHappinessSum2(int[] happiness, int k) {
+        int n = happiness.length, pen = 0;
+        long ans = 0;
+        Arrays.sort(happiness);
+        for(int i = n - 1; i > (n - 1 - k); i--){
+            Integer tp = happiness[i];
+            tp -= pen;
+            tp = Math.max(0, tp);
+            ans += tp;
+            pen++;
+        }
+        return ans;
+    }
+
+
+
+    public static class Pair{
+        public int l;
+        public int r;
+
+        public Pair(int l, int r) {
+            this.l = l;
+            this.r = r;
+        }
+
+        @Override
+        public String toString() {
+            return "Pair{" +
+                    "l=" + l +
+                    ", r=" + r +
+                    '}';
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            Pair pair = (Pair) o;
+            return l == pair.l && r == pair.r;
+        }
+
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(l, r);
+        }
+
+
+
+
+
+    }
+
+    static class PairComparator implements Comparator<Pair> {
+        public int compare(Pair p1, Pair p2) {
+            // Compare based on the first element of the pair
+            if (p1.l != p2.l) {
+                return Integer.compare(p1.l, p2.l);
+            }
+            // If first elements are equal, compare based on the second element
+            return Integer.compare(p1.r, p2.r);
+        }
+    }
+
+    //220. Contains Duplicate III
+    public boolean containsNearbyAlmostDuplicate(int[] nums, int indexDiff, int valueDiff) {
+        int n = nums.length;
+        TreeSet<Pair> set = new TreeSet<>(new PairComparator());
+        int st = 0, end = 0;
+        for(int i = 0 ; i < n; i++){
+            while(end < n && end <= i + indexDiff){
+                set.add(new Pair(nums[end], end));
+                end++;
+            }
+
+            while(st < i - indexDiff){
+                set.remove(new Pair(nums[st], st));
+                st++;
+            }
+            set.remove(new Pair(nums[i], i));
+
+            Pair h = set.floor(new Pair(nums[i] + valueDiff, i));
+            Pair l = set.ceiling(new Pair(nums[i] - valueDiff, i));
+
+            if(h != null && h.r != i && Math.abs(h.l - nums[i]) <= valueDiff) return true;
+            if(l != null && l.r != i && Math.abs(l.l - nums[i]) <= valueDiff) return true;
+
+            set.add(new Pair(nums[i], i));
+
+        }
+
+        return false;
+    }
+
 }
